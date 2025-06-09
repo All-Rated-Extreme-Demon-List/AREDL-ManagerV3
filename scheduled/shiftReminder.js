23e7e57611e3ee6eb206ce5f5dc5663d3659738e
sphericle <86196223+sphericle@users.noreply.github.com>
2025-06-09 00:13:38 -0400

    third commit

---
+++ scheduled/shiftReminder.js
@@ -0,0 +1,63 @@
+const { enableShiftReminders, shiftReminderExpireThreshold, sendShiftRemindersSchedule, apiToken } = require('../config.json');
+const logger = require('log4js').getLogger();
+const { api } = require("../api.js");
+const { EmbedBuilder, Embed } = require('discord.js'); 
+
+module.exports = {
+	name: 'sendShiftReminders',
+	cron: sendShiftRemindersSchedule,
+	enabled: enableShiftReminders,
+	async execute() {
+        const { client } = require('../index.js');
+        logger.log("Scheduled - Sending shift reminders")
+        let shiftsResponse = await api.send(
+            "/aredl/shifts", 
+            "GET", 
+            { per_page: 999, status: "Running" },
+            undefined,
+            apiToken
+        )
+
+        if (shiftsResponse.error) {
+            logger.error(`Scheduled - Error getting shifts: status ${shiftsResponse.status}\n${shiftsResponse.data.message}`)
+        }
+
+        let platShiftsResponse = await api.send(
+            "/arepl/shifts", 
+            "GET", 
+            { per_page: 999, status: "Running" },
+            undefined,
+            apiToken
+        )
+
+        if (platShiftsResponse.error) {
+            logger.error(`Scheduled - Error getting platformer shifts: status ${platShiftsResponse.status}\n${platShiftsResponse.data.message}`)
+        }
+
+        const shifts = [...shiftsResponse.data.data, ...platShiftsResponse.data.data];
+        let now = new Date()
+
+        for (const shift of shifts) {
+            let endDate = new Date(shift.end_at)
+            let timeUntilEnd = endDate - now
+            let hours = shiftReminderExpireThreshold * 60 * 60 * 1000
+            if (timeUntilEnd <= hours && timeUntilEnd > 0) {
+                const userResponse = await api.send(`/users/${shift.user.id}`, 'GET')
+                if (userResponse.error) {
+                    logger.error(`Error fetching user data: ${userResponse.data.message}`);
+                    return;
+                }
+                // unix epochs
+			    let endSeconds = Math.floor(endDate / 1000)
+
+                let embed = new EmbedBuilder()
+                    .setColor(0xf59842)
+                    .setTitle(":warning: Shift Reminder")
+                    .setDescription(`Your shift will expire <t:${endSeconds}:R>!`)
+                    .setTimestamp()
+
+                client.users.cache.get(userResponse.data.discord_id).send({ embeds: [embed] })
+            }
+        }
+    }
+}
