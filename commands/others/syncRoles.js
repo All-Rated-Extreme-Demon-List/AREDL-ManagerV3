192b20289eae74daf4f8c9693310163b7683530f
sphericle <jackb2033@gmail.com>
2025-10-19 12:25:25 -0400

    optionally disable stacking, move object and role adding logic to functions

--- commands/others/syncRoles.js
+++ commands/others/syncRoles.js
@@ -15,71 +15,92 @@ module.exports = {
 	enabled: true,
 	data: new SlashCommandBuilder()
 		.setName('syncroles')
-		.setDescription('Sync all roles from your profile to your account'),
+		.setDescription('Sync all roles from your profile to your account')
+        .addNumberOption(option => 
+            option.setName('stack')
+                .setDescription('For points, pack, and top level roles, ')
+                .setChoices([
+                    { name: 'On', value: 1 },
+                    { name: 'Off', value: 0 },
+                ])
+        ),
 	async execute(interaction) {
         await interaction.deferReply({ flags: MessageFlags.Ephemeral });
-        const member = interaction.member;
+        // do not stack if stack is explicitly set to "off", otherwise stack
+        const shouldStack = interaction.options.getNumber('stack') !== 0;
 
         // placeholder, sphericle's user id on the dev api
         const userId = "71dcba84-55e6-48cf-a826-08ad1bf15abd";
         const profileReq = await api.send(`/aredl/profile/${userId}`);
         if (profileReq.error) {
             logger.error("Sync roles - Error fetching profile:", profileReq.data.message);
-            return interaction.editReply(`Error: ${profileReq.data.message}`);
+            return interaction.editReply(`Error fetching profile: ${profileReq.data.message}`);
         }
         const profile = profileReq.data;
-
         const addedRoles = []
 
-        const beforeRoles = await member.roles.cache.map((role) => role.id);
-        const serverRoles = interaction.guild.roles.cache;
+        // remove all roles to account for different stacking preferences
+        // otherwise stacked roles will never be removed automatically
+        // if the user later decides to resync without stacking
+        const rolesToRemove = [
+            ...Object.values(pointsRoleIDs),
+            ...Object.values(packRoleIDs),
+            ...Object.values(topLevelRoleIDs),
+            extremeGrinderRoleID,
+            opinionPermsRoleID,
+            creatorRoleID,
+            verifierRoleID
+        ]
+        await interaction.member.roles.remove(rolesToRemove);
 
-        const addRole = (roleId) => {
-            if (!serverRoles.hasAny(roleId)) {
-                logger.warn(`Role sync - Role ${roleId} not found in server`)
-                return
-            }
-            // avoid adding a role the user already has
-            if (!beforeRoles.includes(roleId)) {
-                member.roles.add(roleId);
+        const addRoles = (roleIds) => {
+            for (const roleId of roleIds) {
+                if (!interaction.guild.roles.cache.hasAny(roleId)) {
+                    logger.warn(`Role sync - Role ${roleId} not found in server`)
+                    return
+                }
+                interaction.member.roles.add(roleId);
                 addedRoles.push(`- <@&${roleId}>`);
+            }
+        }
+
+        const processRoleType = (roleData, requirement) => {
+            const rolesToAdd = [];
+            Object.entries(roleData).forEach(([k, v]) => {
+                // if the key is not a number, assume it's a string
+                if (requirement(!isNaN(Number(k)) ? parseInt(k) : k)) rolesToAdd.push(v);
+            })
+            if (rolesToAdd.length === 0) return;
+            if (shouldStack) {
+                addRoles(rolesToAdd);
+            } else {
+                // only add the last role in the array
+                addRoles([rolesToAdd[rolesToAdd.length - 1]]);
             }
         }
         
         // Points roles
-        Object.entries(pointsRoleIDs).forEach(([points, roleId]) => {
-            if (profile.rank.total_points >= parseInt(points)) {
-                addRole(roleId)
-            }
-        })
+        processRoleType(pointsRoleIDs, (req) => profile.rank.total_points >= req);
         // Pack roles
-        Object.entries(packRoleIDs).forEach(([packCount, roleId]) => {
-            if (profile.packs.length >= parseInt(packCount)) {
-                addRole(roleId)
-            }
-        })
+        processRoleType(packRoleIDs, (req) => profile.packs.length >= req);
         // Top level roles
         const records = [...profile.records, ...profile.verified];
-        Object.entries(topLevelRoleIDs).forEach(([rank, roleId]) => {
-            if (records.some(record => record.level.position <= parseInt(rank))) {
-                addRole(roleId)
-            }
-        })
+        processRoleType(topLevelRoleIDs, (req) => records.some(record => record.level.position <= req));
         // Opinion perms
         if (profile.rank.extremes >= 10) {
-            addRole(roleId)
+            addRoles([opinionPermsRoleID])
         }
         // Creator role
         if (profile.created.length > 0) {
-            addRole(roleId)
+            addRoles([creatorRoleID])
         }
         // Verifier role
         if (profile.verified.length > 0) {
-            addRole(roleId)
+            addRoles([verifierRoleID])
         }
         // Extreme Grinder role
         if (profile.rank.extremes >= 50) {
-            addRole(roleId)
+            addRoles([extremeGrinderRoleID])
         }
 
         const container = new ContainerBuilder().setAccentColor(0x00ff00);
