e342dac396e8d414a8c46f5547c5b807f2a21105
sphericle <jackb2033@gmail.com>
2025-12-12 12:18:54 -0500

    a lot of logging

--- others/shiftNotifs.js
+++ others/shiftNotifs.js
@@ -3,33 +3,58 @@ const logger = require('log4js').getLogger();
 const { EmbedBuilder } = require('discord.js');
 
 module.exports = {
+    /**
+     * 
+     * @param {import('discord.js').GuildBasedChannel} channel 
+     */
     sendShiftNotif: async (channel, shift, db, shiftID) => {
+        logger.log("Shift notif sending")
+        logger.log(`Shift:`)
+        logger.log(shift)
+        logger.log(`Channel: ${channel.id} ${channel.name}`)
+        logger.log(`shiftID: ${shiftID}`)
+
         try {
+            logger.log(`Fetching reviewer from ${shift.user_id}`)
             const reviewerResponse = await api.send(
                 `/users/${shift.user_id}`,
                 'GET',
             );
+            logger.log(`Response: `)
+            logger.log(reviewerResponse)
             if (reviewerResponse.error) {
                 logger.error(
                     `Shift Notification - Error fetching reviewer ${shift.user_id}: ${reviewerResponse.data.message}`,
                 );
                 await db.shiftNotifs.destroy({ where: { id: shiftID } });
+                logger.log("Destroyed shift notif")
                 return 1;
             }
+            logger.log("Declaring pingStr");
             let pingStr;
+            logger.log(`pingStr: ${pingStr}`)
             if (reviewerResponse.data.discord_id) {
+                logger.log(`Discord ID found: ${reviewerResponse.data.discord_id}`)
                 const settings = await db.settings.findOne({
                     where: {
                         user: reviewerResponse.data.discord_id,
                     },
                 });
+                logger.log("Settings fetched")
                 if (!settings || settings.shiftPings === true) {
+                    logger.log("user wants to be pinged")
                     pingStr = `<@${reviewerResponse.data.discord_id}>`;
+                    logger.log(`New ping str: ${pingStr}`)
                 }
+            } else {
+                logger.log("Discord ID not found")
             }
             // Get unix timestamps for the Discord embed
             const startDate = Math.floor(new Date(shift.start_at).getTime() / 1000);
             const endDate = Math.floor(new Date(shift.end_at).getTime() / 1000);
+
+            logger.log(`Start date: ${startDate}`)
+            logger.log(`End date: ${endDate}`)
 
             const archiveEmbed = new EmbedBuilder()
                 .setColor(0x8fce00)
@@ -44,13 +69,22 @@ module.exports = {
                 ])
                 .setTimestamp();
 
-            await channel.send({ content: pingStr, embeds: [archiveEmbed] });
+            logger.log(`Created embed:`)
+            logger.log(archiveEmbed.data)
+            if (!channel.isSendable()) {
+                logger.error("Channel is not sendable, cancelling...")
+                return 1
+            }
+            const message = await channel.send({ content: pingStr, embeds: [archiveEmbed] });
+            logger.log(`Sent message: ${message.id} ${message.guild.id}`)
             await db.shiftNotifs.destroy({ where: { id: shiftID } });
+            logger.log("Destroyed shift notifs entry")
             return 0;
         } catch (e) {
             logger.error(`Shift Notification - Error sending shift notification: ${e}`);
             logger.error(shift);
             await db.shiftNotifs.destroy({ where: { id: shiftID } });
+            logger.log("Destroyed shift notifs entry")
             return 1;
         }
     },
--- websocket/shifts_created.js
+++ websocket/shifts_created.js
@@ -1,4 +1,5 @@
 const logger = require('log4js').getLogger();
+const { Client } = require('discord.js');
 const {
     guildId,
     staffGuildId,
@@ -9,30 +10,50 @@ const { sendShiftNotif } = require('../others/shiftNotifs.js');
 
 module.exports = {
     notification_type: 'SHIFTS_CREATED',
+    /**
+     * @param {Client<true>} client 
+     */
     async handle(client, data) {
+        logger.log("-".repeat(25))
         logger.log('Received shift created notification:', data);
         if (!shiftsStartedID) return;
+        logger.log(`Shifts started ID is defined: ${shiftsStartedID}`)
         const { db } = require('../index.js');
+        logger.log("Db has been imported")
+        
 
-        const guild = await client.guilds.fetch(guildId);
+        const guild = client.guilds.cache.get(guildId);
+        logger.log(`Guild found: ${guild.id} ${guild.name}`)
         const staffGuild = enableSeparateStaffServer
             ? await client.guilds.fetch(staffGuildId)
             : guild;
+        logger.log(`Staff guild found: ${staffGuild.id} ${staffGuild.name}`)
         const channel = staffGuild.channels.cache.get(shiftsStartedID);
+        logger.log(`Shifts channel found: ${channel.id} ${channel.name}`)
 
         for (const shift of data) {
+            logger.log(`Processing shift from moderator ${shift.user_id} ${shift.start_at} - ${shift.end_at}`)
             const dbShift = await db.shiftNotifs.create({
                 user_id: shift.user_id,
                 start_at: shift.start_at,
                 end_at: shift.end_at,
                 target_count: shift.target_count,
             });
+            logger.log(`Created shift in db`)
+            logger.log(JSON.stringify(dbShift))
             const currentTime = new Date().getTime();
+            logger.log(`Current time: ${currentTime}`)
             const startAt = new Date(shift.start_at);
+            logger.log(`startAt: ${startAt.toISOString()}`)
+
+            logger.log(`Shift sending in ${Math.max(startAt.getTime() - currentTime, 0)} ms`)
         
-            setTimeout(() => {
+            const timeout = setTimeout(() => {
                 sendShiftNotif(channel, shift, db, dbShift.id);
+                logger.log("Timeout callback completed")
             }, Math.max(startAt.getTime() - currentTime, 0));
+
+            logger.log(`Timeout created: ${timeout}`)
         }
     },
 };
