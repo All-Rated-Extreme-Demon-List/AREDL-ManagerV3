e24852d6262bdab20ff63c8fa0f4ae45c2bc6764
sphericle <jackb2033@gmail.com>
2025-12-20 17:59:06 -0500

    feat(syncroles): staff command to sync another user's roles

---
+++ commands/server/syncFor.js
@@ -0,0 +1,42 @@
+
+const { syncRoles } = require("./syncRoles.js")
+const {
+    SlashCommandBuilder,
+    ChatInputCommandInteraction,
+    MessageFlags
+} = require("discord.js");
+
+module.exports = {
+    enabled: true,
+    cooldown: 5,
+    data: new SlashCommandBuilder()
+        .setName('syncfor')
+        .setDescription("Sync roles for another user")
+        .addUserOption((option) => 
+            option
+                .setName("user")
+                .setDescription("The user to sync the roles of")
+                .setRequired(true)
+        )
+        .addNumberOption((option) =>
+            option
+                .setName('stack')
+                .setDescription('Whether to stack points, pack, and top level roles')
+                .setChoices([
+                    { name: 'On', value: 1 },
+                    { name: 'Off', value: 0 },
+                ]),
+        ),
+    /**
+     * @param {ChatInputCommandInteraction} interaction 
+     */
+    async execute(interaction) {
+        await interaction.deferReply({ flags: MessageFlags.Ephemeral });
+        return await syncRoles(
+            interaction,
+            interaction.guild.members.cache.get(
+                interaction.options.getUser("user").id
+            )
+        );
+    },
+};
