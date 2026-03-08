5abbb69ab49c2a109295b816ab6a852e76e6db3d
Minebox260 <joellascoumes@gmail.com>
2026-03-08 03:00:11 +0100

    fix: removed disallowed intent

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,7 @@ import { Client, Collection } from "discord.js";
 import "dotenv/config";
 
 const client = new Client({
-    intents: ["Guilds", "GuildMembers", "GuildMessages", "MessageContent"],
+    intents: ["Guilds", "GuildMembers", "GuildMessages",],
 });
 
 // Initialize websockets Collection for WebSocket handlers
