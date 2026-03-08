0fa995d5160137679d236957bfeee0cb71344292
sphericle <86196223+sphericle@users.noreply.github.com>
2026-03-08 01:11:18 -0500

    idk

--- src/app.ts
+++ src/app.ts
@@ -10,28 +10,3 @@ const client = new Client({
 client.websockets = new Collection();
 
 export default client;
-
-async function removeAllGlobalCommands() {
-    const commands = await client.application?.commands.fetch();
-    if (commands) {
-        for (const [id, command] of commands) {
-            await client.application?.commands.delete(id);
-        }    
-    }
-}
-
-async function removeAllGuildCommands(guildToRemove: string) {
-    const guild = await client.guilds.fetch(guildToRemove);
-    if (guild) {
-        const commands = await guild.commands.fetch();
-        if (commands) {
-            for (const [id, command] of commands) {
-                await guild.commands.delete(id);
-            }
-        }
-    }
-}
-
-removeAllGlobalCommands();
-removeAllGuildCommands(guildId);
-removeAllGuildCommands(staffGuildId);
